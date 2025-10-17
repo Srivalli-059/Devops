b409086390a610d7dcc1687bf8214d6aa139494a
SRIVALLI MEDIKONDA <323103282059.srivalli@gvpcew.ac.in>
2025-10-17 10:00:42 +0530

    Create SumOfTwoNumbers.java

---
+++ SumOfTwoNumbers.java
@@ -0,0 +1,11 @@
+// Simple program to add two numbers in Java
+public class SumOfTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 10;  // first number
+        int num2 = 20;  // second number
+
+        int sum = num1 + num2;  // calculate sum
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
